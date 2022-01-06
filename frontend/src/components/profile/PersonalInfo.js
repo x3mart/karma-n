90a1ca2959289c473008e9012f20426069526f40
viperovm <viperovm@gmail.com>
2022-01-06 12:49:55 +0300

    front added

--- frontend/src/components/profile/PersonalInfo.js
+++ frontend/src/components/profile/PersonalInfo.js
@@ -80,21 +80,21 @@ const PersonalInfo = ({ user, update_action, add_phone_action }) => {
   }
 
   const handlePhoneAddActivate = () => {
-      setExtraPhoneAddActive(true)
+    setExtraPhoneAddActive(true)
   }
   const handlePhoneAddSave = () => {
-      add_phone_action(extraPhone)
-      setExtraPhoneAddActive(false)
+    add_phone_action(extraPhone)
+    setExtraPhoneAddActive(false)
   }
   const handlePhoneAddReset = () => {
-      setExtraPhone('')
-      setExtraPhoneAddActive(false)
+    setExtraPhone('')
+    setExtraPhoneAddActive(false)
   }
 
   return (
     <Fragment>
       <div className='d-flex justify-content-between'>
-        <div>
+        <div className='personal-info-heading'>
           <h1>Личная информация</h1>
         </div>
         {!edit && (
@@ -103,9 +103,9 @@ const PersonalInfo = ({ user, update_action, add_phone_action }) => {
             style={{ cursor: 'pointer' }}
             onClick={() => handleEdit(true)}
           >
-            <div style={{ fontSize: 30, lineHeight: '42px' }}>
+            {/* <div style={{ fontSize: 30, lineHeight: '42px' }}>
               <i className='pe-7s-note' aria-hidden='true' />
-            </div>
+            </div> */}
             <div
               style={{ lineHeight: '42px', margin: '0 0 20px 5px' }}
               className='d-none d-xl-block'
@@ -150,91 +150,82 @@ const PersonalInfo = ({ user, update_action, add_phone_action }) => {
         )}
       </div>
 
-      <div className='row pl-3' style={{ color: '#333' }}>
-        <div className='col-md-3 inner-row-left'>Имя:</div>
-        <div className='col-md-9 col-md-9 inner-row-right'>
-          <input
-            disabled={!edit}
-            name='full_name'
-            className='personal-info-input'
-            type='text'
-            value={userData.full_name}
-            onChange={handleUserUpdate}
-          />
+      <div className='personal-info-wrapper'>
+        <div className='row pl-3'>
+          <div className='col-md-4 inner-row-left'>Имя:</div>
+          <div className='col-md-8 inner-row-right'>
+            <input
+              disabled={!edit}
+              name='full_name'
+              className='personal-info-input'
+              type='text'
+              value={userData.full_name}
+              onChange={handleUserUpdate}
+            />
+          </div>
+        </div>
+        <div className='row pl-3'>
+          <div className='col-md-4 inner-row-left'>Имя пользователя:</div>
+          <div className='col-md-8 inner-row-right'>
+            <input
+              disabled={!edit}
+              name='name'
+              className='personal-info-input'
+              type='text'
+              value={userData.name}
+              onChange={handleUserUpdate}
+            />
+          </div>
+        </div>
+        <div className='row pl-3'>
+          <div className='col-md-4 inner-row-left'>Город:</div>
+          <div className='col-md-8 inner-row-right'>
+            <input
+              disabled={!edit}
+              name='city'
+              className='personal-info-input'
+              type='text'
+              value={userData.city}
+              onChange={handleUserUpdate}
+            />
+          </div>
+        </div>
+        <div className='row pl-3'>
+          <div className='col-md-4 inner-row-left'>Дата рождения:</div>
+          <div className='col-md-8 inner-row-right'>
+            <input
+              disabled={!edit}
+              name='birthday'
+              className='personal-info-input'
+              type='date'
+              value={userData.birthday}
+              onChange={handleUserUpdate}
+            />
+          </div>
+        </div>
+        <div className='row pl-3'>
+          <div className='col-md-4 inner-row-left'>Email:</div>
+          <div className='col-md-8 inner-row-right'>
+            <input
+              disabled={!edit}
+              name='email'
+              className='personal-info-input'
+              type='email'
+              value={userData.email}
+              onChange={handleUserUpdate}
+            />
+          </div>
         </div>
       </div>
-      <div className='row pl-3' style={{ color: '#333' }}>
-        <div className='col-md-3 inner-row-left'>Имя пользователя:</div>
-        <div className='col-md-9 inner-row-right'>
-          <input
-            disabled={!edit}
-            name='name'
-            className='personal-info-input'
-            type='text'
-            value={userData.name}
-            onChange={handleUserUpdate}
-          />
-        </div>
-      </div>
-      <div className='row pl-3' style={{ color: '#333' }}>
-        <div className='col-md-3 inner-row-left'>Город:</div>
-        <div className='col-md-9 inner-row-right'>
-          <input
-            disabled={!edit}
-            name='city'
-            className='personal-info-input'
-            type='text'
-            value={userData.city}
-            onChange={handleUserUpdate}
-          />
-        </div>
-      </div>
-      <div className='row pl-3' style={{ color: '#333' }}>
-        <div className='col-md-3 inner-row-left'>Дата рождения:</div>
-        <div className='col-md-9 inner-row-right'>
-          <input
-            disabled={!edit}
-            name='birthday'
-            className='personal-info-input'
-            type='date'
-            value={userData.birthday}
-            onChange={handleUserUpdate}
-          />
-        </div>
-      </div>
-      <div className='row pl-3' style={{ color: '#333' }}>
-        <div className='col-md-3 inner-row-left'>Email:</div>
-        <div className='col-md-9 inner-row-right'>
-          <input
-            disabled={!edit}
-            name='email'
-            className='personal-info-input'
-            type='email'
-            value={userData.email}
-            onChange={handleUserUpdate}
-          />
-        </div>
-      </div>
-      <div className='row pl-3' style={{ color: '#333' }}>
-        <div className='col-md-3 inner-row-left'>Основной телефон:</div>
-        <div className='col-md-9 inner-row-right'>
-          <input
-            disabled
-            name='phone'
-            className='personal-info-input'
-            type='tel'
-            value={user && user.phone}
-          />
-        </div>
-      </div>
+
       {userData &&
         userData.phones.length > 0 &&
         userData.phones.map((item, index) => (
           <div className='row pl-3' style={{ color: '#333' }}>
-            <div className='col-md-3 inner-row-left'>
-              {index === 0 ? 'Дополнительные телефоны:' : ''}
+            <div className='col-md-4 inner-row-left'>
+              {index === 0 ? 'Телефоны:' : ''}
             </div>
-            <div className='col-md-9 inner-row-right'>
+            <div className='col-md-8 inner-row-right'>
               <input
                 disabled
                 name='phones'
@@ -247,10 +238,10 @@ const PersonalInfo = ({ user, update_action, add_phone_action }) => {
         ))}
 
       <div className='row pl-3' style={{ color: '#333' }}>
-        <div className='col-md-3 inner-row-left'>
-          {userData.phones.length === 0 ? 'Дополнительные телефоны:' : ''}
+        <div className='col-md-4 inner-row-left'>
+          {userData.phones.length === 0 ? 'Телефоны:' : ''}
         </div>
-        <div className='col-md-9 inner-row-right d-flex'>
+        <div className='col-md-8 inner-row-right d-flex'>
           <input
             name='phones'
             className={`w-auto mr-3 personal-info-input ${
@@ -266,14 +257,11 @@ const PersonalInfo = ({ user, update_action, add_phone_action }) => {
               style={{ cursor: 'pointer' }}
               onClick={handlePhoneAddActivate}
             >
-              <div style={{ fontSize: 30, marginTop: '5px' }}>
-                <i className='pe-7s-plus' aria-hidden='true' />
-              </div>
               <div
                 style={{ lineHeight: '42px', margin: '0 0 20px 5px' }}
                 className='d-none d-xl-block'
               >
-                Добавить
+                Добавить телефон
               </div>
             </div>
           ) : (
--- frontend/src/layouts/InnerLayout.js
+++ frontend/src/layouts/InnerLayout.js
@@ -1,38 +1,31 @@
-import React, {Fragment} from "react";
-import MetaTags from "react-meta-tags";
-import {BreadcrumbsItem} from "react-breadcrumbs-dynamic";
-import MainLayout from "./MainLayout";
-import Breadcrumb from "../wrappers/breadcrumb/Breadcrumb";
+import React, { Fragment } from 'react'
+import MetaTags from 'react-meta-tags'
+import { BreadcrumbsItem } from 'react-breadcrumbs-dynamic'
+import MainLayout from './MainLayout'
+import Breadcrumb from '../wrappers/breadcrumb/Breadcrumb'
 
-
-
-const InnerLayout = ({pathname, meta_tags, title, sidebar, content}) => {
-
+const InnerLayout = ({ pathname, meta_tags, title, sidebar, content }) => {
   return (
     <Fragment>
-      <MetaTags>
-        {meta_tags}
-      </MetaTags>
-      <BreadcrumbsItem to={process.env.PUBLIC_URL + "/"}>Главная</BreadcrumbsItem>
+      <MetaTags>{meta_tags}</MetaTags>
+      <BreadcrumbsItem to={process.env.PUBLIC_URL + '/'}>
+        Главная
+      </BreadcrumbsItem>
       <BreadcrumbsItem to={process.env.PUBLIC_URL + pathname}>
         {title}
       </BreadcrumbsItem>
-      <MainLayout headerTop="visible">
-        <Breadcrumb/>
+      <MainLayout headerTop='visible'>
+        <Breadcrumb />
 
-        <div className="container bootstrap snippets bootdey">
-          <div className="row">
-            <div className="profile-nav col-md-6 col-xl-3">
-              {sidebar}
-            </div>
-            <div className="profile-info col-md-6 col-xl-9">
-              {content}
-            </div>
+        <div className='container bootstrap snippets bootdey'>
+          <div className='row'>
+            <div className='user-sidebar-layout'>{sidebar}</div>
+            <div className='user-body-layout'>{content}</div>
           </div>
         </div>
       </MainLayout>
     </Fragment>
-  );
-};
+  )
+}
 
-export default InnerLayout;
+export default InnerLayout
