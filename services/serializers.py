c7b30d593d578908fb8ee7083aee2a69afd6b1e6
x3mart <x3mat@gmail.com>
2022-01-06 13:54:14 +0300

    services and catecories exclude is_active in serializers

--- services/serializers.py
+++ services/serializers.py
@@ -7,11 +7,12 @@ from services.models import Service, ServiceCategory
 class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Service
-        fields = '__all__'
+        exclude = ('is_active',)
+        # extra_kwargs = {'category': {'write_only': True}, }
 
 
 class ServiceCategorySerializer(serializers.ModelSerializer):
     services = ServiceSerializer(many=True, read_only=True)
     class Meta:
         model = ServiceCategory
-        fields = '__all__'
+        exclude = ('is_active',)
