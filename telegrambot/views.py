5cadbb95535b721a0a70ca283436d847431130cf
x3mart <x3mat@gmail.com>
2022-01-23 00:02:43 +0300

    tgbot

--- telegrambot/views.py
+++ telegrambot/views.py
@@ -9,12 +9,13 @@ from rest_framework.renderers import JSONRenderer
 from .serializers import *
 
 class InlineButton:
-    def __init__(self, text, url='', callback_data='', login_url=None, switch_inline_query=None):
+    def __init__(self, text, url=None, callback_data=None, login_url=None, switch_inline_query=None):
         self.text = text
-        self.url = url
-        self.callback_data = callback_data
         self.login_url = login_url
+        self.callback_data = callback_data
         self.switch_inline_query = switch_inline_query
+        if url:
+            self.url = url
 
 
 class KeyboardButton():
@@ -40,13 +41,15 @@ class Update():
 def tg_update_handler(request):
     # print(request.META)
     tgdata = request.data
-    button1 = InlineButton(text='Привет', url='show_user_review 2')
-    button2 = InlineButton(text='Пока', url='show_user_review 2')
+    button1 = InlineButton(text='Привет', callback_data='show_user_review 2')
+    button2 = InlineButton(text='Пока', callback_data='show_user_review 2')
     keyboard = [[button1, button2], [button2]]
     reply_markup = ReplyMarkup()
     reply_markup.inline_keyboard = keyboard
     reply_markup_data = ReplyMarkupSerializer(reply_markup).data
     reply_markup_json = JSONRenderer().render(reply_markup_data)
+    print(reply_markup_data)
+    print(reply_markup_json)
     data = {"chat_id":1045490278, "text": f"<pre><code class='language-python'>{tgdata}</code></pre> \n Вот тут крутое сообщение!!! \n \n <a href='https://novosti247.ru/api/reviews/'> Coll message!!! </a>", "parse_mode":"HTML","reply_markup":reply_markup_json}
     method = "sendMessage"
     response = requests.post(TG_URL + method, data)
