27d4ff4603f189afc2da3cb7330771f419a15c19
x3mart <x3mat@gmail.com>
2022-01-26 20:28:40 +0300

    tg bot callback

--- telegrambot/views.py
+++ telegrambot/views.py
@@ -10,11 +10,14 @@ import requests
 from django.template.loader import render_to_string
 import json
 from rest_framework.renderers import JSONRenderer
+
+from reviewables.models import Reviewable
 from .serializers import *
 from .models import *
+from reviews.models import Review
 
 
-COMMANDS_LIST = ('review', 'user_info')
+COMMANDS_LIST = ('reviews', 'user_info', 'start')
 
 class SendMessage():
     def __init__(self, chat_id, text, parse_mode='HTML', reply_markup=None) -> None:
@@ -39,9 +42,9 @@ class Update():
             else:
                 self.__setattr__(key, value)
     
-    def command_handler(self, message):
-        if message.text.startswith('/'):
-            command_message = message.text.split(' ')
+    def command_handler(self, text):
+        if text.startswith('/'):
+            command_message = text.split(' ')
             command = command_message.pop(0).replace('/', '')
             if command in COMMANDS_LIST:
                 return (command, command_message)
@@ -49,31 +52,43 @@ class Update():
             return (None, [])
     
     def message_dispatcher(self):
-        command, args = self.command_handler(self.message)
+        command, args = self.command_handler(self.message.text)
+        if command == 'start':
+            text = render_to_string('start.html', {'reviews_count': Review.objects.count(), 'reviewable_count': Reviewable.objects.count()})
+            button1 = InlineButton(text='Инфа о себе', callback_data=f'/user_info 2')
+            button2 = InlineButton(text='Отзывы о Вас', callback_data=f'/reviews 2')
+            keyboard = [[button1], [button2]]
+            reply_markup = ReplyMarkup()
+            reply_markup.inline_keyboard = keyboard
+            reply_markup_data = ReplyMarkupSerializer(reply_markup).data
+            reply_markup_json = JSONRenderer().render(reply_markup_data)
+            response = SendMessage(chat_id=self.message.chat.id, text=text, reply_markup=reply_markup_json).send()
+        else:
+            text = "No commands"
+            response = SendMessage(chat_id=self.message.chat.id, text=text).send()
+        return response
+        # chat_id = self.message.chat.id
+        # button1 = InlineButton(text=f'Привет  {self.message.chat.username}', callback_data=f'Привет {self.message.chat.username}')
+    
+    def callback_dispatcher(self):
+        command, args = self.command_handler(self.callback_query.data)
         if command == 'user_info':
-            text = render_to_string('user_info.html', {'user': Account.objects.get(pk=2)})
-            # text = 'command'
+            text = render_to_string('user_info.html', {'user': Account.objects.get(pk=int(args[0]))})
         else:
             text = "No commands"
         response = SendMessage(chat_id=self.message.chat.id, text=text).send()
         return response
-        # chat_id = self.message.chat.id
-        # button1 = InlineButton(text=f'Привет  {self.message.chat.username}', callback_data=f'Привет {self.message.chat.username}')
 
 
 @api_view(["POST", "GET"])
 @permission_classes((permissions.AllowAny,))
 def tg_update_handler(request):
-    # tgdata = request.data 
-    # send_message = SendMessage(chat_id=1045490278, text=f"{tgdata}")
-    # data = SendMessageSerializer(send_message).data
-    # response = requests.post(TG_URL + "sendMessage", data)
     update = Update(request.data)
     if hasattr(update,'message'):
         result = update.message_dispatcher()
-        SendMessage(chat_id=1045490278, text=result.json).send()
-    # # elif hasattr(update,'callback_query'):
-    # #     pass
+        # SendMessage(chat_id=1045490278, text=result.json).send()
+    elif hasattr(update,'callback_query'):
+        result = update.callback_dispatcher()
     #     # method = "answerCallbackQuery"
     #     # callback_aswer = AnswerCallbackQuery(callback_query_id = update.callback_query.id, text = update.callback_query.data, show_alert=True)
     #     # callback_aswer_data = AnswerCallbackQuerySerializer(callback_aswer).data
