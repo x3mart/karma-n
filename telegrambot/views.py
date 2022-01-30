b60913570d9dd7f0b576b835435ebb1d175bdc6f
x3mart <x3mat@gmail.com>
2022-01-30 04:04:52 +0300

    tg bot reviews

--- telegrambot/views.py
+++ telegrambot/views.py
@@ -319,10 +319,10 @@ def tg_update_handler(request):
             update.message_dispatcher()
         elif hasattr(update,'callback_query'):
             update.callback_dispatcher()
-        method = "sendMessage"
-        send_message = SendMessage(chat_id=1045490278, text=f'{request.data}')
-        data = SendMessageSerializer(send_message).data
-        requests.post(TG_URL + method, data)
+        # method = "sendMessage"
+        # send_message = SendMessage(chat_id=1045490278, text=f'{request.data}')
+        # data = SendMessageSerializer(send_message).data
+        # requests.post(TG_URL + method, data)
     except:
         pass
     return Response({}, status=200)
