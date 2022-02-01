995a41aada3830b021ade7cf85edc05b01ac4d54
x3mart <x3mat@gmail.com>
2022-02-01 18:12:24 +0300

    get code phone clean

--- utils/reviewables.py
+++ utils/reviewables.py
@@ -1,5 +1,5 @@
 def clean_phone(phone_number):
     phone_number = phone_number.replace(' ','').replace('(','').replace(')','').replace('-','').replace('+','')
-    if not phone_number.startwith('7'):
-        pass
+    if phone_number.startswith('8'):
+        phone_number = "7" + phone_number[1:]
     return phone_number
