a3ec34cccc696ac1735124cce3d88503212e7a0d
x3mart <x3mat@gmail.com>
2022-06-30 18:43:38 +0300

    user avatar

--- checks/views.py
+++ checks/views.py
@@ -168,7 +168,7 @@ def check_code(request):
 def vk_handler(request):
     secret = request.data.get('secret', None)
     if secret == VK_SECRET:
-        return Response({'f85e17c9'},status=200)
+        return Response('f85e17c9',status=200, content_type='text/plain')
     if request.data.get('group_id') == 209432771:
         return Response('f85e17c9',status=200)
     return HttpResponseBadRequest()
